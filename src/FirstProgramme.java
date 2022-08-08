43fcccca0d8144d9f86488695adfaef31186a0bb
sriramsurya <sriramsurya95@gmail.com>
2022-08-08 13:12:55 +0100

    this is a test

---
+++ src/FirstProgramme.java
@@ -0,0 +1,14 @@
+
+ class FirstProgramme
+ {
+	public static void main(String args[]) 
+	{
+	 int i=20;
+	 if (i%2==0)
+	
+		System.out.print("Even");
+		else 
+			System.out.println("odd");
+	
+	}
+}
